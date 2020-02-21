1e084ac37ec4186ebd1da11616a7c538158dc2d5
Marco Santiago <marco.aurelio@rcky.com.br>
2020-02-21 10:53:03 -0300

    Ajuste de importações de reducers

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -3,12 +3,10 @@ import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import TabReducer from '../common/tab/tabReducer'
-import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 import Alerts from '../alerts/alertReducer';
 
 const rootReducer = combineReducers({
     tab: TabReducer,
-    billingCycle: BillingCycleReducer,
     form: formReducer,
     toastr: toastrReducer,
     Alerts: Alerts,
